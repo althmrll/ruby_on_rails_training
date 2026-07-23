461a90079451c870629ac75493d26503a825e995
Althea De Lara <adelara.technavyphil@gmail.com>
2026-07-23 13:35:39 +0800

    feat: created a simple ruby script that applies input/ output and control flow.

--- ruby.rb
+++ ruby.rb
@@ -1,1 +1,13 @@
-#Initial Ruby File
+#Ruby script using input/output and control flow
+
+puts "What is your name?"
+name = gets.chomp
+puts "What is your age?"
+age=gets.chomp
+remaining_years=21-age
+
+unless age>=21
+  puts "Conrats " + name + "! You're an adult in all countries"
+else
+  puts "Sadly, you still have " + remaining_years + " years remaining before you become an adult."
+end
