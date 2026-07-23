9944d3df7b9163f97e7429695b4aef13c30d997f
Althea De Lara <adelara.technavyphil@gmail.com>
2026-07-23 13:45:22 +0800

    fix:  switched commit messages as saying I'm 20 gives out congratulatory message.

--- ruby.rb
+++ ruby.rb
@@ -4,10 +4,11 @@ puts "What is your name?"
 name = gets.chomp
 puts "What is your age?"
 age=gets.chomp
+
 remaining_years=21-age.to_i
 
 unless age.to_i>=21
-  puts "Conrats " + name + "! You're an adult in all countries"
+  puts "Sadly, you still have " + remaining_years.to_s + " year/s remaining before you become an adult."
 else
-  puts "Sadly, you still have " + remaining_years.to_s + " years remaining before you become an adult."
+  puts "Congrats " + name + "! You're an adult in all countries"
 end
