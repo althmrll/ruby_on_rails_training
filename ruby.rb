5d9e54d9fc6b02572347fec461cf19c826d6010e
Althea De Lara <adelara.technavyphil@gmail.com>
2026-07-23 13:37:30 +0800

    fix: added converters to convert sring to number for age and back to string for the output message

--- ruby.rb
+++ ruby.rb
@@ -4,10 +4,10 @@ puts "What is your name?"
 name = gets.chomp
 puts "What is your age?"
 age=gets.chomp
-remaining_years=21-age
+remaining_years=21-age.to_i
 
-unless age>=21
+unless age.to_i>=21
   puts "Conrats " + name + "! You're an adult in all countries"
 else
-  puts "Sadly, you still have " + remaining_years + " years remaining before you become an adult."
+  puts "Sadly, you still have " + remaining_years.to_s + " years remaining before you become an adult."
 end
