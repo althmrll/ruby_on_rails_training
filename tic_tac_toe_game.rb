dd5fa617e9395125c5ecea2fdcf3fcda3ca829bd
Althea De Lara <adelara.technavyphil@gmail.com>
2026-07-28 08:30:47 +0800

    feat: added main menu

--- tic_tac_toe_game.rb
+++ tic_tac_toe_game.rb
@@ -1,5 +1,20 @@
 #tic-tac-toe-game
+#ruby tic_tac_toe_game.rb
+
 #Start menu: ask user to start the game
+def start_menu
+  puts "Welcome to Tic-Tac-Toe!\n"
+  puts "Would you like to start the game? (Y/N)"
+  answer = gets.chomp
+  answer.upcase!
+  
+  case answer
+  when "Y" then puts "Game Start!"
+  else
+    puts "Goodbye!"
+  end
+end
+
 #start game (x goes first always)
 #Print the board
 #Ask current player to input where they want to put their marks
@@ -7,3 +22,5 @@
 #alternate between players until someone wins or the board gets full
 #gameplay summary (?)
 #ask if they want to replay or go back to main menu.
+
+start_menu
