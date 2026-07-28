e08fed9bf14e8457331791c98232afffd78dfc53
Althea De Lara <adelara.technavyphil@gmail.com>
2026-07-28 08:21:31 +0800

    feat: created pseudocode for tic-tac-toe

---
+++ tic_tac_toe_game.rb
@@ -0,0 +1,9 @@
+#tic-tac-toe-game
+#Start menu: ask user to start the game
+#start game (x goes first always)
+#Print the board
+#Ask current player to input where they want to put their marks
+#Print updated board
+#alternate between players until someone wins or the board gets full
+#gameplay summary (?)
+#ask if they want to replay or go back to main menu.
