e7ddcb32f4602ca07f10d24e711cc904227e2af9
Althea De Lara <adelara.technavyphil@gmail.com>
2026-07-28 16:26:19 +0800

    refactor: added winning lines

--- tic_tac_toe_game.rb
+++ tic_tac_toe_game.rb
@@ -4,6 +4,7 @@
 @grid = [" "," "," "," "," "," "," "," "," "," "]
 @game_events=[]
 @occupied=0
+@wins=[[1,2,3],[4,5,6],[7,8,9],[1,5,9],[3,5,7],[1,4,5],[2,5,8],[3,6,9]]
 
 #Start menu: ask user to start the game
 def start_menu
